d5922e4eb8e18c614aabfbaa3c1e2af8a84227ef
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-17 16:35:16 -0500

    getting selected movie using hooks

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,11 +1,34 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
 
-class Movies extends Component {
-    render(){
-        return(
-            <h1>Movies Page</h1>
-        )
+const Movies=()=>{
+    const [ movies, setMovies ] = useState([]);
+    const [ selectedMovie, setSelectedMovie ] = useState({});
+
+    useEffect(()=>{
+        fetch("https://ghibliapi.herokuapp.com/films")
+            .then(res=>res.json())
+            .then(data=>{
+                setMovies(data);
+            })
+    }, [])
+
+    const handleDropdownChange=(e)=>{
+        console.log(e.target.value)
+        // setSelectedMovie()
     }
+
+    let optionElArr = movies.map((movie)=>{
+        return <option value={movie.title}>{movie.title}</option>
+    })
+
+    return(
+        <div className="movies">
+            <select onChange={handleDropdownChange}>
+                <option></option>
+                { optionElArr }
+            </select>
+        </div>
+    )
 }
 
 export default Movies;
