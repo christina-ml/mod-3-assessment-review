5a1ae1bb55af0e2f93db000227ac19867a0b4c2e
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-21 11:39:08 -0500

    shows not found or the name, age, gender.

--- src/components/People.js
+++ src/components/People.js
@@ -1,11 +1,55 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 
-class People extends Component {
-    render(){
-        return(
-            <h1>People Page</h1>
-        )
+const People=()=>{
+    const [ people, setPeople ] = useState([]);
+    const [ personInput, setPersonInput ] = useState('')
+    const [ foundPerson, setFoundPerson ] = useState(null)  // null or empty object - null to start off as falsy
+
+    useEffect(()=>{
+        fetch("https://ghibliapi.herokuapp.com/people")
+            .then(res=>res.json())
+            .then((data)=>{
+                setPeople(data)
+            })
+    }, []);
+
+    const handlePersonInput=(e)=>{
+        setPersonInput(e.target.value)
     }
+
+    const handleSubmit=()=>{
+        let result = people.find(person=>{
+            return person.name.toLowerCase() === personInput.toLowerCase();
+        })
+        setFoundPerson(result);
+    }
+
+    return (
+        <div>
+            <div>Search for a Person</div>
+            <input 
+                placeholder="FInd Your Person"
+                type="text"
+                value={ personInput }
+                onChange={ handlePersonInput }
+            />
+            <button onClick={ handleSubmit }>Submit</button>
+
+            { foundPerson && 
+                <div>
+                    <h3>Name: {foundPerson.name}</h3>
+                    <h3>Age: {foundPerson.age}</h3>
+                    <h3>Gender: {foundPerson.gender}</h3>
+                </div>
+            }
+            { foundPerson === undefined && 
+                <div>
+                    <h3>Not Found</h3>
+                </div>
+            }
+
+        </div>
+    )
 }
 
 export default People;
